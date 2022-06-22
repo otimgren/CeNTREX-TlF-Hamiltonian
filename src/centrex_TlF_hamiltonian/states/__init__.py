0cc9e81b3c714c62daceafee41799ab28f5b4124
ograsdijk <o.grasdijk@gmail.com>
2022-06-21 19:18:24 -0500

    add population functions

--- src/centrex_TlF_hamiltonian/states/__init__.py
+++ src/centrex_TlF_hamiltonian/states/__init__.py
@@ -16,9 +16,13 @@ from .find_states import *
 from . import constants
 from .constants import *
 
+from . import population
+from .population import *
+
 __all__ = states.__all__.copy()
 __all__ += utils.__all__.copy()
 __all__ += utils_compact.__all__.copy()
 __all__ += generate_states.__all__.copy()
 __all__ += find_states.__all__.copy()
 __all__ += constants.__all__.copy()
+__all__ += population.__all__.copy()
